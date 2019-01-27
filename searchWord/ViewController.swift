b94e376add71a2563f1c3afa8c60c82eaa4b9893
俞佳兴 <albertyu@ys-macbook-pro.local>
2019-01-27 22:28:06 +0800

    Initial Commit

---
+++ searchWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  searchWord
+//
+//  Created by 俞佳兴 on 2019/1/27.
+//  Copyright © 2019 Albert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
